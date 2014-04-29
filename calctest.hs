ded49071f5c4355f730e2b24cb346f18cf093479
Richard Glew <richard.glew@gmail.com>
2014-04-29 12:20:59 +0000

    Boost coverage

--- calctest.hs
+++ calctest.hs
@@ -9,6 +9,10 @@ main =  runTestTT tests
 test1 = TestCase (assertEqual "baseprice" (6.0) (baseprice 2.0 3.0))
 test2 = TestCase (assertEqual "stateTax" 1.08 (statetax "NV" 1))
 test3 = TestCase (assertEqual "discount" 9000.00 (applydiscount 10000))
+test4 = TestCase (assertEqual "discount" 43350.00 (applydiscount 51000))
+test5 = TestCase (assertEqual "statetax" 1.0625 (statetax "TX" 1))
+test6 = TestCase (assertEqual "statetax" 1.04 (statetax "AL" 1))
+test7 = TestCase (assertEqual "statetax" 1.0825 (statetax "CA" 1))
 
-tests = TestList [TestLabel "baseprice" test1, test2, test3]
+tests = TestList [TestLabel "baseprice" test1, test2, test3, test4, test5, test6]
 
